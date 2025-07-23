31fb235f83119c06a7f48e217d1faed108d6a332
Md. Abu Hanif <abu.hanif@transcombd.com>
2025-07-23 10:29:25 +0600

    kill

---
+++ kill all process in SQL server.sql
@@ -0,0 +1,11 @@
+USE [master];
+
+DECLARE @kill varchar(8000) = '';  
+SELECT @kill = @kill + 'kill ' + CONVERT(varchar(5), session_id) + ';'  
+FROM sys.dm_exec_sessions
+WHERE database_id  in ('6')
+and session_id>'50'
+and session_id<>(SELECT @@SPID AS CurrentSPID)
+and session_id<>('70')
+and [status] in ('running','sleeping')
+EXEC(@kill);
